892c7f7059a8e1d20c451455c55828f55844efd6
vincdcle <vincent.de.clercq@hexagon.com>
2022-11-10 16:01:16 +0100

    add new files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          BIERRRRR.
         </p>
         <a
           className="App-link"
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn react
         </a>
+        <p> schollekes </p>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders schol', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/schol/i);
+  expect(linkElement).toBeInTheDocument();
+});
